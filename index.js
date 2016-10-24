0ac47fd4842c1fd1f2f61de0c2a9a801ee35d796
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2016-10-25 02:33:47 +0300

    added procfile

--- index.js
+++ index.js
@@ -27,9 +27,11 @@ io.on('connection', function(socket){
 		io.emit('chat message', socket.name + " changed name to " + user);
 		console.log(socket.name + " changed name to " + user);
 		socket.name = user;
-	})
+	});
 });
 
-http.listen(3000, function(){
-	console.log("listening to port 3000");
+var port = Number(process.env.PORT || 3000);
+
+http.listen(port, function(){
+	console.log('listening on *:3000');
 });

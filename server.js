a19c4ca887539811f6b51b7695815306ec409228
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2016-10-25 03:25:29 +0300

    muutoksii

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var users = 0;
 
 io.on('connection', function(socket){
 	users++;
-	socket.name = "Neekeri" + users;
+	socket.name = "Kaveri" + users;
 	console.log(socket.name + " connected");
 	io.emit('chat message', socket.name + " connected");
 	socket.on('disconnect', function(){

f38977fac752305bb9eabfbdacdd2f4461adf633
Robert Kuhlmann <robert.kuhlmann2@gmail.fi>
2016-10-27 01:44:08 +0300

    userlist released!

--- public/script/chatscript.js
+++ public/script/chatscript.js
@@ -1,9 +1,22 @@
 var socket = io();
 
 $('#typemsg').submit(function(){
-  	socket.emit('chat message', $('#msg').val());
-  	$('#msg').val('');
-	return false;
+	var input = $('#msg').val();
+
+	switch(input){
+		case "": socket.emit('status msg yellow', "You're message was blank...");
+		$('#msg').val('');
+		return false;
+		break;
+		case "/users": socket.emit('cmd users');
+		$('#msg').val('');
+		return false;
+		break;
+		default: socket.emit('chat message', $('#msg').val());
+		$('#msg').val('');
+		return false;
+		break;
+	}
 });
 
 socket.on('chat message', function(msg){
@@ -11,29 +24,37 @@ socket.on('chat message', function(msg){
   	$('#messages').animate({scrollTop: $('#messages').prop("scrollHeight")}, 100);
 });
 
-socket.on('change name', function(msg){
+socket.on('status msg green', function(msg){
   	$("#messages").append($('<li class="list-group-item list-group-item-success">').text(msg));
   	$('#messages').animate({scrollTop: $('#messages').prop("scrollHeight")}, 100);
 });
 
-socket.on('offline', function(msg){
+socket.on('status msg red', function(msg){
   	$("#messages").append($('<li class="list-group-item list-group-item-danger">').text(msg));
   	$('#messages').animate({scrollTop: $('#messages').prop("scrollHeight")}, 100);
 });
 
-
-
-socket.on('clear users', function(){
-	$('#users').empty();
+socket.on('status msg yellow', function(msg){
+	$("#messages").append($('<li class="list-group-item list-group-item-warning">').text(msg));
+  	$('#messages').animate({scrollTop: $('#messages').prop("scrollHeight")}, 100);
 });
 
-socket.on('set userlist', function(user){
-	$("#users").append($('<li class="list-group-item">').text(user));
-})
-
+socket.on('set users', function(data){
+	$('#users').empty();
+	for(i = 0;i < data.length;i++){
+		$("#users").append($('<li class="list-group-item list-group-item-primary">').text(data[i]));
+	}
+});
 
 $('#username').submit(function(){
-  	socket.emit('change name', $('#user').val());
-  	$('#user').val('');
-  	return false;
+	var input = $('#user').val();
+	if(input == ""){
+		socket.emit('status msg yellow', "Blank username, not cool...");
+		$('#user').val('');
+		return false;
+	} else {
+		socket.emit('change name', $('#user').val());
+		$('#user').val('');
+		return false;
+	}
 });
--- server.js
+++ server.js
@@ -6,29 +6,30 @@ var io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-var users = 0;
 var userList = [];
+var randomNames = ["Donald Trump", "Ismo Laitela", "Obama", "Arnold Scwarzenegger", "Anssi", "Teppo", "Hillary Clinton", "clone man", "Jakob", "Harambe"];
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname, '/index.html');
 });
 
 io.on('connection', function(socket){
-	users++;
-	socket.name = "User" + users;
-	userList.push(socket);
+	//New user
+	socket.name = "Spectator " + randomNames[Math.floor((Math.random() * 10))];
 	console.log(socket.name + " connected");
-	socket.broadcast.emit('change name', socket.name + " connected");
-	socket.emit('change name' ,"Welcome! Current users: " + users);
+	socket.broadcast.emit('status msg green', socket.name + " connected");
+	userList.push(socket);
+	updateUsers();
+	socket.emit('status msg green' ,"Welcome! Start by typing your name!");
+
+	//Disconnect
 	socket.on('disconnect', function(){
-		io.emit('offline', socket.name + " disconnected");
+		io.emit('status msg red', socket.name + " disconnected");
 		userList.splice(userList.indexOf(socket), 1);
-		users--;
+		updateUsers();
 	});
-});
 
-io.on('connection', function(socket){
-
+	//Chat message
 	socket.on('chat message', function(msg){
 		var currentTime = new Date();
 		var today = new Date();  
@@ -44,14 +45,43 @@ io.on('connection', function(socket){
 
 		io.emit('chat message', "[" + d.getHours() + ":" + minutes + "] " + socket.name + " : " + msg);
 	});
+
+	//Status message green
+	socket.on('status msg green', function(msg){
+		io.emit('status msg green', msg);
+	});
+
+	//Status message yellow
+	socket.on('status msg yellow', function(msg){
+		socket.emit('status msg yellow', msg);
+	});
+
+	socket.on('cmd users', function(){
+		var usertext = "Current users: ";
+		for(i=0;i<userList.length;i++){
+			usertext += userList[i].name + " ";
+		}
+		socket.emit('status msg yellow', usertext);
+	})
+
+	//Change username
 	socket.on('change name', function(user){
-		io.emit('change name', socket.name + " changed name to " + user);
+		io.emit('status msg green', socket.name + " changed name to " + user);
 		console.log(socket.name + " changed name to " + user);
 		socket.name = user;
+		userList.splice(userList.indexOf(socket), 1);
+		userList.push(socket);
+		updateUsers();
 	});
+
+	function updateUsers() {
+		var users = [];
+		for(i = 0; i < userList.length; i++){
+			users.push(userList[i].name);
+		}
+		io.emit('set users', users);
+	}
 });
-
-
 
 var port = Number(process.env.PORT || 3000);
 
